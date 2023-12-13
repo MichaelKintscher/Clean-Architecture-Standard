bf2551eb78edce3063d4576ae29d44a39bf8d7fb
Michael Kintscher <michael@kintscher.dev>
2023-12-12 19:31:47 -0700

    Added Entity Project, and navigation event args to Windows project.

---
+++ Domain/Navigation/NavigationRequestedEventArgs.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Navigation
+{
+    /// <summary>
+    /// Contains event info for a navigation request.
+    /// </summary>
+    internal class NavigationRequestedEventArgs : EventArgs
+    {
+        /// <summary>
+        /// The string value representing the page being reqested as the target of the navigation.
+        /// </summary>
+        public string ToPage { get; private set; }
+
+        /// <summary>
+        /// A flag indicating whether this navigation request is a back navigation request or not.
+        /// </summary>
+        public bool IsBackRequest { get; private set; }
+
+        public NavigationRequestedEventArgs(string toPage, bool isBackRequest)
+        {
+            this.ToPage = toPage;
+            this.IsBackRequest = isBackRequest;
+        }
+    }
+}
