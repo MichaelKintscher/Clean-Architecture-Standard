241a40f3caa733d895163bbba9c5c0f966a6d5fe
Michael Kintscher <michael@kintscher.dev>
2024-11-09 01:37:29 -0700

    Added a base class Entity Updated EventArgs, as well as a generic implementation of that base class as Item Updated EventArgs.

---
+++ Clean Architecture Standard/Domain/EventArguments/EntityUpdatedEventArgs.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArchitecture.Core.Domain.EventArguments
+{
+    /// <summary>
+    /// Contains event info with an entity value that has been updated and a list of names of the updated properties on that entity..
+    /// NOT intended for direct use; inherit from this class. 
+    /// </summary>
+    /// <typeparam name="T">The type of the entity the args contains.</typeparam>
+    public abstract class EntityUpdatedEventArgs<T> : EntityEventArgs<T>
+    {
+        private List<string> _updatedProperties;
+        /// <summary>
+        /// A list of names of the properties that have been updated. Empty list if no properties were updated.
+        /// </summary>
+        public List<string> UpdatedProperties
+        {
+            get { return new List<string>(this._updatedProperties); }
+            private set { this._updatedProperties = value; }
+        }
+
+        /// <summary>
+        /// Constructor sets the given value and list of names of properties that have been updated in the given value.
+        /// </summary>
+        /// <param name="value">The value to set.</param>
+        /// <param name="updatedProperties">The list of names of properties that have been updated in the given value.</param>
+        public EntityUpdatedEventArgs(T value, List<string> updatedProperties)
+            : base(value)
+        {
+            // Assign the given list. If the given list is null, assign an empty list.
+            this._updatedProperties = updatedProperties == null ? new List<string>() : new List<string>(updatedProperties);
+        }
+    }
+}
---
+++ Clean Architecture Standard/Domain/EventArguments/ItemUpdatedEventArgs.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArchitecture.Core.Domain.EventArguments
+{
+    /// <summary>
+    /// Contains event info with an updated item.
+    /// Use this as a generic event args. Manual casting of the stored value is needed.
+    /// </summary>
+    public class ItemUpdatedEventArgs : EntityUpdatedEventArgs<object>
+    {
+        /// <summary>
+        /// The updated item.
+        /// </summary>
+        public object Item { get => this.Value; }
+
+        /// <summary>
+        /// Creates a new instance of the event args.
+        /// </summary>
+        /// <param name="item">The item that was updated.</param>
+        /// <param name="updatedProperties">The list of names of properties that have been updated on the item.</param>
+        public ItemUpdatedEventArgs(object item, List<string> updatedProperties) : base(item, updatedProperties) { }
+    }
+}
